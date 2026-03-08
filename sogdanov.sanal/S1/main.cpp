9191732f097f299d7319de47aab28e72e12934a3
kuzRK <sanalsogdanov11188@gmail.com>
2026-03-08 19:31:28 +0300

    implementation transpose

--- sogdanov.sanal/S1/main.cpp
+++ sogdanov.sanal/S1/main.cpp
@@ -34,7 +34,29 @@ namespace sogdanov {
                                                                      std::move(nums)});
     }
   }
-  void transpose(const List_pair& list, List<List<size_t>>& data);
+  void transpose(const List_pair& list, List<List<size_t>>& data) {
+    size_t max_size = 0;
+    for (auto it = list.begin(); it != list.end(); ++it) {
+      if (it->second.size() > max_size) {
+        max_size = it->second.size();
+      }
+    }
+    LIter<List<size_t>> data_last;
+    for (size_t i = 0; i < max_size; ++i) {
+      List<size_t> row;
+      LIter<size_t> row_last;
+      for (auto it - list.begin(); it != list.end(); ++it) {
+        auto elem = it->second.begin();
+        for (size_t j = 0; j < i; ++j) {
+          ++elem;
+        }
+        if (e != it->second.end()) {
+          row_last = list_append(row, row_last, *elem);
+        }
+      }
+      data_last = list_append(data, data_last, std::move(row));
+    }
+  }
   void output(std::ostream& o, const List_pair& names, const List<List<size_t>>& transp);
 
 }
