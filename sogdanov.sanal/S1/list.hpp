23ea03dd2a6f04060ffa5c3a55e6f9f834f0c432
kuzRK <sanalsogdanov11188@gmail.com>
2026-05-25 15:44:46 +0300

    fix CG

--- sogdanov.sanal/S1/list.hpp
+++ sogdanov.sanal/S1/list.hpp
@@ -1,129 +1,208 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+
 #include <cstddef>
 #include <utility>
+
 namespace sogdanov {
-  template<class T>
+
+  template< class T >
   struct Node {
+    Node(const T &d, Node *n);
+    Node(T &&val, Node *n);
+
     T data;
-    Node* next;
-    Node(const T& d, Node* n);
-    Node(T&& val, Node* n);
+    Node *next;
   };
-  template<class T>
+
+  template< class T >
   class List;
-  template<class T>
+
+  template< class T >
   class LCIter;
-  template<class T>
+
+  template< class T >
   class LIter {
-    friend class List<T>;
-    friend class LCIter<T>;
-    Node<T>* ptr_;
   public:
     LIter() noexcept;
-    LIter(Node<T>* p) noexcept;
-    T& operator*() const;
-    LIter& operator++() noexcept;
-    bool operator==(const LIter& o) const noexcept;
-    bool operator!=(const LIter& o) const noexcept;
+    LIter(Node< T > *p) noexcept;
+
+    T &operator*() const;
+    LIter &operator++() noexcept;
+    bool operator==(const LIter &o) const noexcept;
+    bool operator!=(const LIter &o) const noexcept;
+
+  private:
+    friend class List< T >;
+    friend class LCIter< T >;
+
+    Node< T > *ptr_;
   };
-  template<class T>
+
+  template< class T >
   class LCIter {
-    friend class List<T>;
-    friend class LIter<T>;
-    const Node<T>* ptr_;
   public:
     LCIter() noexcept;
-    LCIter(const Node<T>* p) noexcept;
-    LCIter(const LIter<T>& it) noexcept;
-    const T& operator*() const;
-    LCIter& operator++() noexcept;
-    bool operator==(const LCIter& o) const noexcept;
-    bool operator!=(const LCIter& o) const noexcept;
+    LCIter(const Node< T > *p) noexcept;
+    LCIter(const LIter< T > &it) noexcept;
+
+    const T &operator*() const;
+    LCIter &operator++() noexcept;
+    bool operator==(const LCIter &o) const noexcept;
+    bool operator!=(const LCIter &o) const noexcept;
+
+  private:
+    friend class List< T >;
+    friend class LIter< T >;
+
+    const Node< T > *ptr_;
   };
-  template<class T>
+
+  template< class T >
   class List {
-    Node<T>* head_;
-    Node<T>* tail_;
-    size_t size_;
   public:
     List() noexcept;
-    List(const List& other);
-    List(List&& other) noexcept;
-    List& operator=(const List& other);
-    List& operator=( List&& other) noexcept;
+    List(const List &other);
+    List(List &&other) noexcept;
+    List &operator=(const List &other);
+    List &operator=(List &&other) noexcept;
     ~List() noexcept;
-    LIter<T> begin() noexcept;
-    LIter<T> end() noexcept;
-    LCIter<T> begin() const noexcept;
-    LCIter<T> end() const noexcept;
-    void push_front(const T& val);
+
+    LIter< T > begin() noexcept;
+    LIter< T > end() noexcept;
+    LCIter< T > begin() const noexcept;
+    LCIter< T > end() const noexcept;
+
+    void push_front(const T &val);
     void pop_front();
-    LIter<T> insert_after(LIter<T> pos, const T& val);
-    LIter<T> insert_after(LIter<T> pos, T&& val);
+    LIter< T > insert_after(LIter< T > pos, const T &val);
+    LIter< T > insert_after(LIter< T > pos, T &&val);
+
     void clear() noexcept;
     bool empty() const noexcept;
     size_t size() const noexcept;
+
+  private:
+    Node< T > *head_;
+    Node< T > *tail_;
+    size_t size_;
   };
-  template<class T>
-  Node<T>::Node(const T& val, Node* n) : data(val), next(n) {}
-  template<class T>
-  Node<T>::Node(T&& val, Node* n) : data(std::move(val)), next(n) {}
-  template<class T>
-  LIter<T>::LIter() noexcept : ptr_(nullptr) {}
-  template<class T>
-  LIter<T>::LIter(Node<T>* p) noexcept : ptr_(p) {}
-  template<class T>
-  T& LIter<T>::operator*() const {
+
+  template< class T >
+  Node< T >::Node(const T &d, Node *n):
+    data(d),
+    next(n)
+  {
+  }
+
+  template< class T >
+  Node< T >::Node(T &&val, Node *n):
+    data(std::move(val)),
+    next(n)
+  {
+  }
+
+  template< class T >
+  LIter< T >::LIter() noexcept:
+    ptr_(nullptr)
+  {
+  }
+
+  template< class T >
+  LIter< T >::LIter(Node< T > *p) noexcept:
+    ptr_(p)
+  {
+  }
+
+  template< class T >
+  T &LIter< T >::operator*() const
+  {
     return ptr_->data;
   }
-  template<class T>
-  LIter<T>& LIter<T>::operator++() noexcept {
+
+  template< class T >
+  LIter< T > &LIter< T >::operator++() noexcept
+  {
     if (ptr_) {
       ptr_ = ptr_->next;
     }
     return *this;
   }
-  template<class T>
-  bool LIter<T>::operator==(const LIter& o) const noexcept {
+
+  template< class T >
+  bool LIter< T >::operator==(const LIter &o) const noexcept
+  {
     return ptr_ == o.ptr_;
   }
-  template<class T>
-  bool LIter<T>::operator!=(const LIter& o) const noexcept {
+
+  template< class T >
+  bool LIter< T >::operator!=(const LIter &o) const noexcept
+  {
     return ptr_ != o.ptr_;
   }
-  template<class T>
-  LCIter<T>::LCIter() noexcept : ptr_(nullptr) {}
-  template<class T>
-  LCIter<T>::LCIter(const Node<T>* p) noexcept : ptr_(p) {}
-  template<class T>
-  LCIter<T>::LCIter(const LIter<T>& it) noexcept : ptr_(it.ptr_) {}
-  template<class T>
-  const T& LCIter<T>::operator*() const {
+
+  template< class T >
+  LCIter< T >::LCIter() noexcept:
+    ptr_(nullptr)
+  {
+  }
+
+  template< class T >
+  LCIter< T >::LCIter(const Node< T > *p) noexcept:
+    ptr_(p)
+  {
+  }
+
+  template< class T >
+  LCIter< T >::LCIter(const LIter< T > &it) noexcept:
+    ptr_(it.ptr_)
+  {
+  }
+
+  template< class T >
+  const T &LCIter< T >::operator*() const
+  {
     return ptr_->data;
   }
-  template<class T>
-  LCIter<T>& LCIter<T>::operator++() noexcept {
+
+  template< class T >
+  LCIter< T > &LCIter< T >::operator++() noexcept
+  {
     if (ptr_) {
       ptr_ = ptr_->next;
     }
     return *this;
   }
-  template<class T>
-  bool LCIter<T>::operator==(const LCIter& o) const noexcept {
+
+  template< class T >
+  bool LCIter< T >::operator==(const LCIter &o) const noexcept
+  {
     return ptr_ == o.ptr_;
   }
-  template<class T>
-  bool LCIter<T>::operator!=(const LCIter& o) const noexcept {
+
+  template< class T >
+  bool LCIter< T >::operator!=(const LCIter &o) const noexcept
+  {
     return ptr_ != o.ptr_;
   }
-  template<class T>
-  List<T>::List() noexcept : head_(nullptr), tail_(nullptr), size_(0) {}
-  template<class T>
-  List<T>::List(const List& other) : head_(nullptr), tail_(nullptr), size_(0) {
+
+  template< class T >
+  List< T >::List() noexcept:
+    head_(nullptr),
+    tail_(nullptr),
+    size_(0)
+  {
+  }
+
+  template< class T >
+  List< T >::List(const List &other):
+    head_(nullptr),
+    tail_(nullptr),
+    size_(0)
+  {
     try {
-      for (LCIter<T> it = other.begin(); it != other.end(); ++it) {
-        Node<T>* node = new Node<T>(*it, nullptr);
+      for (LCIter< T > it = other.begin(); it != other.end(); ++it) {
+        Node< T > *node = new Node< T >(*it, nullptr);
         if (!tail_) {
           head_ = node;
           tail_ = node;
@@ -138,104 +217,134 @@ namespace sogdanov {
       throw;
     }
   }
-  template<class T>
-  List<T>::List(List&& other) noexcept
-  : head_(other.head_), tail_(other.tail_), size_(other.size_) {
-    other.head_ = other.tail_ = nullptr;
+
+  template< class T >
+  List< T >::List(List &&other) noexcept:
+    head_(other.head_),
+    tail_(other.tail_),
+    size_(other.size_)
+  {
+    other.head_ = nullptr;
+    other.tail_ = nullptr;
     other.size_ = 0;
   }
-  template<class T>
-  List<T>& List<T>::operator=(const List& other) {
-  if (this != &other) {
-    List tmp(other);
-    std::swap(head_, tmp.head_);
-    std::swap(tail_, tmp.tail_);
-    std::swap(size_, tmp.size_);
+
+  template< class T >
+  List< T > &List< T >::operator=(const List &other)
+  {
+    if (this != &other) {
+      List tmp(other);
+      std::swap(head_, tmp.head_);
+      std::swap(tail_, tmp.tail_);
+      std::swap(size_, tmp.size_);
+    }
+    return *this;
   }
-  return *this;
+
+  template< class T >
+  List< T > &List< T >::operator=(List &&other) noexcept
+  {
+    if (this != &other) {
+      clear();
+      head_ = other.head_;
+      tail_ = other.tail_;
+      size_ = other.size_;
+      other.head_ = nullptr;
+      other.tail_ = nullptr;
+      other.size_ = 0;
+    }
+    return *this;
   }
-  template<class T>
-  List<T>& List<T>::operator=(List&& other) noexcept {
-  if (this != &other) {
+
+  template< class T >
+  List< T >::~List() noexcept
+  {
     clear();
-    head_ = other.head_;
-    tail_ = other.tail_;
-    size_ = other.size_;
-    other.head_ = other.tail_ = nullptr;
-    other.size_ = 0;
   }
-  return *this;
+
+  template< class T >
+  LIter< T > List< T >::begin() noexcept
+  {
+    return LIter< T >(head_);
   }
-  template<class T>
-  List<T>::~List() noexcept {
-    clear();
+
+  template< class T >
+  LIter< T > List< T >::end() noexcept
+  {
+    return LIter< T >(nullptr);
   }
-  template<class T>
-  LIter<T> List<T>::begin() noexcept {
-    return LIter<T>(head_);
+
+  template< class T >
+  LCIter< T > List< T >::begin() const noexcept
+  {
+    return LCIter< T >(head_);
   }
-  template<class T>
-  LIter<T> List<T>::end() noexcept {
-    return LIter<T>(nullptr);
+
+  template< class T >
+  LCIter< T > List< T >::end() const noexcept
+  {
+    return LCIter< T >(nullptr);
   }
-  template<class T>
-  LCIter<T> List<T>::begin() const noexcept {
-    return LCIter<T>(head_);
-  }
-  template<class T>
-  LCIter<T> List<T>::end() const noexcept {
-    return LCIter<T>(nullptr);
-  }
-  template<class T>
-  void List<T>::push_front(const T& val) {
-    Node<T>* node = new Node<T>(val, head_);
+
+  template< class T >
+  void List< T >::push_front(const T &val)
+  {
+    Node< T > *node = new Node< T >(val, head_);
     head_ = node;
     if (!tail_) {
       tail_ = head_;
     }
     ++size_;
   }
-  template<class T>
-  void List<T>::pop_front() {
+
+  template< class T >
+  void List< T >::pop_front()
+  {
     if (empty()) {
       return;
     }
-    Node<T>* old = head_;
+    Node< T > *old = head_;
     head_ = head_->next;
     delete old;
     --size_;
   }
-  template<class T>
-  LIter<T> List<T>::insert_after(LIter<T> pos, const T& val) {
+
+  template< class T >
+  LIter< T > List< T >::insert_after(LIter< T > pos, const T &val)
+  {
     if (!pos.ptr_) {
       return end();
     }
-    Node<T>* node = new Node<T>(val, pos.ptr_->next);
+    Node< T > *node = new Node< T >(val, pos.ptr_->next);
     pos.ptr_->next = node;
     if (pos.ptr_ == tail_) {
       tail_ = node;
     }
     ++size_;
-    return LIter<T>(node);
+    return LIter< T >(node);
   }
-  template<class T>
-  LIter<T> List<T>::insert_after(LIter<T> pos, T&& val) {
+
+  template< class T >
+  LIter< T > List< T >::insert_after(LIter< T > pos, T &&val)
+  {
     if (!pos.ptr_) {
       return end();
     }
-    Node<T>* node = new Node<T>(std::move(val), pos.ptr_->next);
+    Node< T > *node = new Node< T >(std::move(val), pos.ptr_->next);
     pos.ptr_->next = node;
     if (pos.ptr_ == tail_) {
-    tail_ = node;
+      tail_ = node;
     }
     ++size_;
-    return LIter<T>(node);
+    return LIter< T >(node);
   }
-  template<class T>
-  void List<T>::clear() noexcept {
-    Node<T>* cur = head_;
+
+  template< class T >
+  void List< T >::clear() noexcept
+  {
+    Node< T > *cur = head_;
     while (cur) {
-      Node<T>* n = cur->next;
+      Node< T > *n = cur->next;
       delete cur;
       cur = n;
     }
@@ -243,13 +352,19 @@ namespace sogdanov {
     tail_ = nullptr;
     size_ = 0;
   }
-  template<class T>
-  bool List<T>::empty() const noexcept {
+
+  template< class T >
+  bool List< T >::empty() const noexcept
+  {
     return head_ == nullptr;
   }
-  template<class T>
-  size_t List<T>::size() const noexcept {
+
+  template< class T >
+  size_t List< T >::size() const noexcept
+  {
     return size_;
   }
+
 }
+
 #endif
--- sogdanov.sanal/S1/main.cpp
+++ sogdanov.sanal/S1/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <string>
 #include <limits>
+#include <string>
+
 #include "list.hpp"
+
 namespace sogdanov {
-  template<class T>
-  LIter<T> list_append(List<T>& list, LIter<T> last, T val) {
+
+  template< class T >
+  LIter< T > list_append(List< T > &list, LIter< T > last, T val)
+  {
     if (list.empty()) {
       list.push_front(std::move(val));
       return list.begin();
     }
     return list.insert_after(last, std::move(val));
   }
-  using List_pair = List<std::pair<std::string, List<size_t>>>;
-  void input(std::istream& in, List_pair& list) {
-    LIter<std::pair<std::string, List<size_t>>> last;
+
+  using List_pair = List< std::pair< std::string, List< size_t > > >;
+
+  void input(std::istream &in, List_pair &list)
+  {
+    LIter< std::pair< std::string, List< size_t > > > last;
     std::string name;
+
     while (in >> name) {
-      List<size_t> nums;
-      LIter<size_t> nums_last;
+      List< size_t > nums;
+      LIter< size_t > nums_last;
       char ch;
+
       while (in.get(ch)) {
         if (ch == '\n') {
           break;
@@ -32,21 +41,26 @@ namespace sogdanov {
           nums_last = list_append(nums, nums_last, val);
         }
       }
-      last = list_append(list, last,std::pair<std::string , List<size_t>>{name, \
-                                                                     std::move(nums)});
+
+      std::pair< std::string, List< size_t > > entry{name, std::move(nums)};
+      last = list_append(list, last, std::move(entry));
     }
   }
-  void transpose(const List_pair& list, List<List<size_t>>& data) {
+
+  void transpose(const List_pair &list, List< List< size_t > > &data)
+  {
     size_t max_size = 0;
     for (auto it = list.begin(); it != list.end(); ++it) {
       if ((*it).second.size() > max_size) {
         max_size = (*it).second.size();
       }
     }
-    LIter<List<size_t>> data_last;
+
+    LIter< List< size_t > > data_last;
     for (size_t i = 0; i < max_size; ++i) {
-      List<size_t> row;
-      LIter<size_t> row_last;
+      List< size_t > row;
+      LIter< size_t > row_last;
+
       for (auto it = list.begin(); it != list.end(); ++it) {
         auto elem = (*it).second.begin();
         for (size_t j = 0; j < i && elem != (*it).second.end(); ++j) {
@@ -59,13 +73,16 @@ namespace sogdanov {
       data_last = list_append(data, data_last, std::move(row));
     }
   }
-  List<size_t> calc_sum(const List<List<size_t>>& transp) {
-    List<size_t> sums;
-    LIter<size_t> sums_last;
+
+  List< size_t > calc_sum(const List< List< size_t > > &transp)
+  {
+    List< size_t > sums;
+    LIter< size_t > sums_last;
+
     for (auto row = transp.begin(); row != transp.end(); ++row) {
       size_t sum = 0;
       for (auto it = (*row).begin(); it != (*row).end(); ++it) {
-        if (sum > std::numeric_limits<size_t>::max() - *it) {
+        if (sum > std::numeric_limits< size_t >::max() - *it) {
           throw std::overflow_error("overflow errror\n");
         }
         sum += *it;
@@ -74,7 +91,9 @@ namespace sogdanov {
     }
     return sums;
   }
-  void output(std::ostream& out, const List_pair& names, const List<List<size_t>>& transp) {
+
+  void output(std::ostream &out, const List_pair &names, const List< List< size_t > > &transp)
+  {
     bool is_name = true;
     for (auto it = names.begin(); it != names.end(); ++it) {
       if (!is_name) {
@@ -84,9 +103,10 @@ namespace sogdanov {
       is_name = false;
     }
     out << '\n';
+
     for (auto row = transp.begin(); row != transp.end(); ++row) {
       bool f = true;
-        for (auto it = (*row).begin(); it != (*row).end(); ++it) {
+      for (auto it = (*row).begin(); it != (*row).end(); ++it) {
         if (!f) {
           out << ' ';
         }
@@ -95,14 +115,16 @@ namespace sogdanov {
       }
       out << '\n';
     }
-    List<size_t> sums = calc_sum(transp);
+
+    List< size_t > sums = calc_sum(transp);
     if (sums.empty()) {
       out << 0 << '\n';
       return;
     }
+
     bool is_sum = true;
     for (auto it = sums.begin(); it != sums.end(); ++it) {
-      if(!is_sum) {
+      if (!is_sum) {
         out << ' ';
       }
       out << *it;
@@ -110,21 +132,27 @@ namespace sogdanov {
     }
     out << '\n';
   }
+
 }
-int main() {
-  using namespace sogdanov;
-  List_pair list;
-  input(std::cin, list);
+
+int main()
+{
+  sogdanov::List_pair list;
+  sogdanov::input(std::cin, list);
+
   if (list.empty()) {
     std::cout << 0 << '\n';
     return 0;
   }
-  List<List<size_t>> transp;
-  transpose(list, transp);
+
+  sogdanov::List< sogdanov::List< size_t > > transp;
+  sogdanov::transpose(list, transp);
+
   try {
-    output(std::cout, list, transp);
-  } catch (const std::overflow_error& e) {
+    sogdanov::output(std::cout, list, transp);
+  } catch (const std::overflow_error &e) {
     std::cerr << e.what() << '\n';
     return 1;
   }
+
 }
